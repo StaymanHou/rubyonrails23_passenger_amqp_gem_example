b02752bc08ae2fc69f5aaef5668542b0c0588e8c
Mark Abramov <markizko@gmail.com>
2011-06-04 21:07:51 +0400

    Removed warmup message publishing

--- config/initializers/amqp.rb
+++ config/initializers/amqp.rb
@@ -1,8 +1,8 @@
 PhusionPassenger.on_event(:starting_worker_process) do |forked|
-  if forked
-    require 'eventmachine'
-    require 'amqp'
+  require 'eventmachine'
+  require 'amqp'
 
+  if forked
     Rails.logger.info "[AMQP] Initializing amqp..."
     amqp_thread = Thread.new {
       failure_handler = lambda {
@@ -14,18 +14,11 @@ PhusionPassenger.on_event(:starting_worker_process) do |forked|
     sleep(0.15)
 
     EventMachine.next_tick do
-      queue_name = "amqpgem.examples.rails23.passenger.warmup"
       AMQP.channel ||= AMQP::Channel.new(AMQP.connection)
-      AMQP.channel.queue(queue_name, :durable => true)
 
-      message = "Warmup message"
-      AMQP.channel.default_exchange.publish(message, :routing_key => queue_name) do
-        Rails.logger.info Terminal.yellow("[AMQP] Published \"#{message}\" to #{queue_name}")
-      end
-
-      other_queue = "amqpgem.examples.rails23.passenger.index"
-      AMQP.channel.queue(other_queue, :durable => true).subscribe do |message|
-        Rails.logger.info Terminal.green("[AMQP] Received \"#{message}\" from #{other_queue}")
+      queue_name = "amqpgem.examples.rails23.passenger.index"
+      AMQP.channel.queue(queue_name, :durable => true).subscribe do |message|
+        Rails.logger.info Terminal.green("[AMQP] Received \"#{message}\" from #{queue_name}")
       end
 
     end
