7bd5c91c38e106cfb7a6cb4b3b6402235c114e23
Mark Abramov <markizko@gmail.com>
2011-06-04 16:14:16 +0400

    Simple controller action publishing message to AMQP in runtime

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+    message = params.inspect
+    @called = 0
+    AMQP.channel.default_exchange.publish(message, :routing_key => "amqpgem.examples.rails23.passenger.index") do
+      Rails.logger.info Terminal.yellow("[AMQP] Published \"#{message}\" #{@called += 1}")
+    end
+    render :text => "Hello, world! Check your terminal log."
+  end # index
+end # class HomeController
