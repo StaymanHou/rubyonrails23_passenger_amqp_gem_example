88f407be9436f1f70eb8dfd835c7fdfbab553fb1
Mark Abramov <markizko@gmail.com>
2011-06-04 21:08:24 +0400

    Removed call counter from controller (the problem was addressed in amq-client)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     message = params.inspect
     @called = 0
     AMQP.channel.default_exchange.publish(message, :routing_key => "amqpgem.examples.rails23.passenger.index") do
-      Rails.logger.info Terminal.yellow("[AMQP] Published \"#{message}\" #{@called += 1}")
+      Rails.logger.info Terminal.yellow("[AMQP] Published \"#{message}\"")
     end
     render :text => "Hello, world! Check your terminal log."
   end # index
