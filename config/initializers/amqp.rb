386d283ec287517c7c18ba17e3841ea9efeed599
Mark Abramov <markizko@gmail.com>
2011-06-27 20:28:46 +0400

    Safeguard in the AMQP passenger hook

--- config/initializers/amqp.rb
+++ config/initializers/amqp.rb
@@ -1,26 +1,28 @@
-PhusionPassenger.on_event(:starting_worker_process) do |forked|
-  require 'eventmachine'
-  require 'amqp'
+if defined?(PhusionPassenger) # otherwise it breaks rake commands if you put this in an initializer
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    require 'eventmachine'
+    require 'amqp'
 
-  if forked
-    Rails.logger.info "[AMQP] Initializing amqp..."
-    amqp_thread = Thread.new {
-      failure_handler = lambda {
-        Rails.logger.fatal Terminal.red("[AMQP] [FATAL] Could not connect to AMQP broker")
+    if forked
+      Rails.logger.info "[AMQP] Initializing amqp..."
+      amqp_thread = Thread.new {
+        failure_handler = lambda {
+          Rails.logger.fatal Terminal.red("[AMQP] [FATAL] Could not connect to AMQP broker")
+        }
+        AMQP.start(:on_tcp_connection_failure => failure_handler)
       }
-      AMQP.start(:on_tcp_connection_failure => failure_handler)
-    }
-    amqp_thread.abort_on_exception = true
-    sleep(0.15)
+      amqp_thread.abort_on_exception = true
+      sleep(0.15)
 
-    EventMachine.next_tick do
-      AMQP.channel ||= AMQP::Channel.new(AMQP.connection)
+      EventMachine.next_tick do
+        AMQP.channel ||= AMQP::Channel.new(AMQP.connection)
 
-      queue_name = "amqpgem.examples.rails23.passenger.index"
-      AMQP.channel.queue(queue_name, :durable => true).subscribe do |message|
-        Rails.logger.info Terminal.green("[AMQP] Received \"#{message}\" from #{queue_name}")
+        queue_name = "amqpgem.examples.rails23.passenger.index"
+        AMQP.channel.queue(queue_name, :durable => true).subscribe do |message|
+          Rails.logger.info Terminal.green("[AMQP] Received \"#{message}\" from #{queue_name}")
+        end
+
       end
-
     end
   end
 end
